af9abde8658a5c6011037f9432eadb670974105d
Sunjay Varma <varma.sunjay@gmail.com>
2019-10-12 11:45:44 -0400

    Making sure we get the actual length of the line read from getline

--- dino-std/src/dbstr.rs
+++ dino-std/src/dbstr.rs
@@ -139,10 +139,19 @@ pub extern fn read_line_bstr() -> DBStr {
     //TODO: Free memory allocated by getline
     // See: http://man7.org/linux/man-pages/man3/getline.3.html
     let mut data = ptr::null_mut();
-    let mut length = 0;
+    // Note that the allocated buffer length can often be much bigger than the actual length of
+    // characters on the line
+    let mut buffer_len = 0usize;
     //TODO: lock stdin
-    if unsafe { getline(&mut data as *mut _, &mut length as *mut _, super::stdin) } == -1 {
+    let length = unsafe { getline(&mut data as *mut _, &mut buffer_len as *mut _, super::stdin) };
+    if length == -1 {
         //TODO: Error handling
     }
+
+    // Ignore the trailing newline. Note that because the newline is always read, `length` is
+    // guaranteed to be greater than or equal to 1 before this line.
+    let length = (length - 1) as usize;
+    //TODO: We should truncate the data buffer to save memory if buffer_len >> length
+
     return DBStr {data: data as *mut u8, length}
 }
