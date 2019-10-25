d64503c925a2d5aab3e21acce55b5513c46ca0a3
Sunjay Varma <varma.sunjay@gmail.com>
2019-10-24 21:57:26 -0400

    Moved function code generation loop into its own function

--- src/trans.rs
+++ src/trans.rs
@@ -6,7 +6,8 @@ mod function;
 use snafu::Snafu;
 
 use crate::ir;
-use crate::resolve::ProgramDecls;
+use crate::resolve::{ProgramDecls, DeclMap};
+use crate::primitives::Primitives;
 use crate::codegen::*;
 
 use function::FunctionCodeGenerator;
@@ -23,20 +24,34 @@ pub enum Error {
 /// Generates an executable program from the given IR
 pub fn executable(prog: &ir::Program, program_scope: &ProgramDecls) -> Result<CExecutableProgram, Error> {
     let ir::Program {top_level_module} = prog;
-    let ir::Module {functions: mod_functions} = top_level_module;
+    let ir::Module {functions} = top_level_module;
 
     let ProgramDecls {top_level_decls: mod_scope, prims} = program_scope;
 
     let mut entry_point = None;
-    let mut functions = Vec::new();
+    let functions = gen_functions(functions, mod_scope, prims, &mut entry_point)?;
 
-    for func in mod_functions {
+    let entry_point = match entry_point {
+        Some(entry_point) => entry_point,
+        None => return Err(Error::NoEntryPoint),
+    };
+
+    Ok(CExecutableProgram {functions, entry_point})
+}
+
+fn gen_functions(
+    functions: &[ir::Function],
+    mod_scope: &DeclMap,
+    prims: &Primitives,
+    entry_point: &mut Option<CEntryPoint>,
+) -> Result<Vec<CFunction>, Error> {
+    functions.iter().filter_map(|func| {
         // A "main" function in the top level declarations of a program must be the entry point
         if func.name == "main" {
             let ir::Function {sig, ..} = func;
             // The main function must have no return type and no arguments
             if sig.return_type != prims.unit() || !sig.params.is_empty() {
-                return Err(Error::InvalidEntryPointType);
+                return Some(Err(Error::InvalidEntryPointType));
             }
 
             // Note that it is guaranteed that `entry_point` will only be assigned once since
@@ -45,18 +60,17 @@ pub fn executable(prog: &ir::Program, program_scope: &ProgramDecls) -> Result<CE
             debug_assert!(entry_point.is_none(), "bug: allowed multiple entry points");
 
             // Take the generated body and put it in the right struct
-            let CFunction {sig: _, body} = FunctionCodeGenerator::generate(func, mod_scope)?;
-            entry_point = Some(CEntryPoint {body});
+            let CFunction {sig: _, body} = match FunctionCodeGenerator::generate(func, mod_scope) {
+                Ok(func) => func,
+                Err(err) => return Some(Err(err)),
+            };
+            *entry_point = Some(CEntryPoint {body});
+
+            // Filter this function out
+            None
 
         } else {
-            functions.push(FunctionCodeGenerator::generate(func, mod_scope)?);
+            Some(FunctionCodeGenerator::generate(func, mod_scope))
         }
-    }
-
-    let entry_point = match entry_point {
-        Some(entry_point) => entry_point,
-        None => return Err(Error::NoEntryPoint),
-    };
-
-    Ok(CExecutableProgram {functions, entry_point})
+    }).collect()
 }
