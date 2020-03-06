3c2d54c0a21bb8be379de59535ab8ea042eebedb
Sunjay Varma <varma.sunjay@gmail.com>
2020-03-06 16:35:28 -0500

    Hiding diagnostics in tests through new NullWriter

--- src/diagnostics.rs
+++ src/diagnostics.rs
@@ -1,11 +1,64 @@
-use std::io::Write;
+use std::io::{self, Write};
+use std::borrow::Cow;
 use std::sync::Mutex;
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 use termcolor::{StandardStream, ColorChoice, ColorSpec, Color, WriteColor};
 
+trait DiagnosticsWriter {
+    fn write_error(&mut self, message: &str) -> io::Result<()>;
+    fn write_warning(&mut self, message: &str) -> io::Result<()>;
+}
+
+impl DiagnosticsWriter for StandardStream {
+    fn write_error(&mut self, message: &str) -> io::Result<()> {
+        let mut out = self.lock();
+
+        out.set_color(ColorSpec::new().set_fg(Some(Color::Red)).set_bold(true))?;
+        write!(out, "error: ")?;
+        out.reset()?;
+
+        writeln!(out, "{}", message)
+    }
+
+    fn write_warning(&mut self, message: &str) -> io::Result<()> {
+        let mut out = self.lock();
+
+        out.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)).set_bold(true))?;
+        write!(out, "warning: ")?;
+        out.reset()?;
+
+        writeln!(out, "{}", message)
+    }
+}
+
+#[cfg(test)]
+struct NullWriter;
+
+#[cfg(test)]
+impl NullWriter {
+    pub fn new(_color_choice: ColorChoice) -> Self {
+        // This impl exists to silence an unused parameter warning
+        NullWriter
+    }
+}
+
+#[cfg(test)]
+impl DiagnosticsWriter for NullWriter {
+    fn write_error(&mut self, _message: &str) -> io::Result<()> {
+        Ok(())
+    }
+
+    fn write_warning(&mut self, _message: &str) -> io::Result<()> {
+        Ok(())
+    }
+}
+
 pub struct Diagnostics {
+    #[cfg(not(test))]
     out: Mutex<StandardStream>,
+    #[cfg(test)]
+    out: Mutex<NullWriter>,
     /// The number of errors that have been emitted
     errors: AtomicUsize,
     /// The number of warnings that have been emitted
@@ -15,7 +68,10 @@ pub struct Diagnostics {
 impl Diagnostics {
     pub fn new(color_choice: ColorChoice) -> Self {
         Self {
+            #[cfg(not(test))]
             out: Mutex::new(StandardStream::stderr(color_choice)),
+            #[cfg(test)]
+            out: Mutex::new(NullWriter::new(color_choice)),
             errors: AtomicUsize::default(),
             warnings: AtomicUsize::default(),
         }
@@ -34,28 +90,16 @@ impl Diagnostics {
 
 //TODO: Create the real diagnostics API
 impl Diagnostics {
-    pub fn emit_error(&self, message: impl Into<String>) {
-        let out = self.out.lock().expect("lock poisoned");
-        let mut out = out.lock();
-
-        out.set_color(ColorSpec::new().set_fg(Some(Color::Red)).set_bold(true)).expect("IO Error");
-        write!(out, "error: ").expect("IO Error");
-        out.reset().expect("IO Error");
-
-        writeln!(out, "{}", message.into()).expect("IO Error");
+    pub fn emit_error<'a>(&self, message: impl Into<Cow<'a, str>>) {
+        let mut out = self.out.lock().expect("lock poisoned");
+        out.write_error(message.into().as_ref()).expect("IO Error");
 
         self.errors.fetch_add(1, Ordering::SeqCst);
     }
 
-    pub fn emit_warning(&self, message: impl Into<String>) {
-        let out = self.out.lock().expect("lock poisoned");
-        let mut out = out.lock();
-
-        out.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)).set_bold(true)).expect("IO Error");
-        write!(out, "warning: ").expect("IO Error");
-        out.reset().expect("IO Error");
-
-        writeln!(out, "{}", message.into()).expect("IO Error");
+    pub fn emit_warning<'a>(&self, message: impl Into<Cow<'a, str>>) {
+        let mut out = self.out.lock().expect("lock poisoned");
+        out.write_warning(message.into().as_ref()).expect("IO Error");
 
         self.warnings.fetch_add(1, Ordering::SeqCst);
     }
