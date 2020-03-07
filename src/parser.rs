c2192de6b0389b9510bdb9d20088de4b3005dc14
Sunjay Varma <varma.sunjay@gmail.com>
2020-03-07 15:26:33 -0500

    Incomplete work on parser

--- src/parser.rs
+++ src/parser.rs
@@ -21,6 +21,13 @@ pub fn parse_module<'a>(source: &'a str, diag: &'a Diagnostics) -> Module<'a> {
     parser.module()
 }
 
+/// An unrecoverable parse error
+///
+/// Many errors are recoverable, so we can still return `Ok` for those, this is when we want to
+/// return `Err` from a function.
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+struct Unrecoverable;
+
 struct ModuleParser<'a> {
     lexer: Lexer<'a>,
     diag: &'a Diagnostics,
@@ -30,7 +37,7 @@ impl<'a> ModuleParser<'a> {
     fn module(&mut self) -> Module<'a> {
         let mut decls = Vec::new();
         while self.lexer.peek().kind != Eof {
-            if let Some(decl) = self.decl() {
+            if let Ok(decl) = self.decl() {
                 decls.push(decl);
             }
         }
@@ -38,32 +45,101 @@ impl<'a> ModuleParser<'a> {
         Module {decls}
     }
 
-    fn decl(&mut self) -> Option<Decl<'a>> {
+    fn decl(&mut self) -> Result<Decl<'a>, Unrecoverable> {
         use Keyword::*;
-        Some(match self.lexer.peek().kind {
+        Ok(match self.lexer.peek().kind {
             Keyword(Use) => Decl::Import(self.use_decl()?),
-            Keyword(Struct) => Decl::Struct(self.struct_decl()),
+            Keyword(Struct) => Decl::Struct(self.struct_decl()?),
             Keyword(Impl) => Decl::Impl(self.impl_decl()),
             Keyword(Fn) => Decl::Function(self.func_decl()),
             _ => {
                 let token = self.lexer.next();
                 self.unexpected_token(token, &[Keyword(Use), Keyword(Struct),
                     Keyword(Impl), Keyword(Fn)]);
-                return None;
+                return Err(Unrecoverable);
             },
         })
     }
 
-    fn use_decl(&mut self) -> Option<ImportPath<'a>> {
+    fn use_decl(&mut self) -> Result<ImportPath<'a>, Unrecoverable> {
         // The `use` token
         self.lexer.next();
 
-        self.import_path()
+        let path = self.import_path()?;
+
+        self.match_next(Semicolon)?;
+
+        Ok(path)
     }
 
-    fn struct_decl(&mut self) -> Struct<'a> {
+    fn import_path(&mut self) -> Result<ImportPath<'a>, Unrecoverable> {
+        let mut components = Vec::new();
+        let selection = loop {
+            let token = self.lexer.next();
+            let component = match &token.kind {
+                Star => break Ok(ImportSelection::All),
+                OpenDelim(Delim::Brace) => {
+                    let names = self.import_names_list()?;
+                    break Ok(ImportSelection::Names(names));
+                },
+                _ => self.path_component(token)?,
+            };
+            components.push(component);
+
+            let token = self.lexer.next();
+            match &token.kind {
+                DoubleColon => continue,
+                Semicolon => break Err(Unrecoverable),
+                _ => {
+                    self.unexpected_token(token, &[DoubleColon, Semicolon]);
+                    return Err(Unrecoverable);
+                },
+            }
+        };
+
+        Ok(ImportPath {path: IdentPath {components}, selection})
+    }
+
+    fn import_names_list(&mut self) -> Result<Vec<ast::Ident<'a>>, Unrecoverable> {
+        let mut names = Vec::new();
+
+        loop {
+            let token = self.lexer.next();
+            match &token.kind {
+                CloseDelim(Delim::Brace) => break,
+                Ident => {
+                    let ident = token.to_str()
+                        .expect("bug: idents should be valid utf-8 by definition");
+                    names.push(ident);
+                },
+                _ => {
+                    self.unexpected_token(token, &[CloseDelim(Delim::Brace), Ident]);
+                    return Err(Unrecoverable);
+                },
+            }
+        }
+        while self.lexer.peek().kind != CloseDelim(Delim::Brace) {
+            let token = self.match_next(Ident)?;
+
+            let token = self.lexer.next();
+            match &token.kind {
+                Comma => continue,
+                CloseDelim(Delim::Brace) => break,
+                _ => {
+                    self.unexpected_token(token, &[DoubleColon, Semicolon]);
+                    return Err(Unrecoverable);
+                },
+            }
+        }
+
+        Ok(names)
+    }
+
+    fn struct_decl(&mut self) -> Result<Struct<'a>, Unrecoverable> {
         // The `struct` token
         self.lexer.next();
+
+        let name_token = self.match_next(Ident)?;
         todo!()
     }
 
@@ -79,49 +155,7 @@ impl<'a> ModuleParser<'a> {
         todo!()
     }
 
-    fn import_path(&mut self) -> Option<ImportPath<'a>> {
-        let mut components = Vec::new();
-        let selection = loop {
-            let token = self.lexer.next();
-            let component = match &token.kind {
-                Star => break Some(ImportSelection::All),
-                OpenDelim(Delim::Brace) => {
-                    let names = self.import_names_list()?;
-                    self.match_next(CloseDelim(Delim::Brace));
-                    break Some(ImportSelection::Names(names));
-                },
-                _ => self.path_component(token)?,
-            };
-            components.push(component);
-
-            let token = self.lexer.next();
-            match &token.kind {
-                DoubleColon => continue,
-                Semicolon => break None,
-                _ => {
-                    self.unexpected_token(token, &[DoubleColon, Semicolon]);
-                    return None;
-                },
-            }
-        };
-
-        Some(ImportPath {path: IdentPath {components}, selection})
-    }
-
-    fn import_names_list(&mut self) -> Option<Vec<ast::Ident<'a>>> {
-        let mut names = Vec::new();
-
-        while self.lexer.peek().kind != CloseDelim(Delim::Brace) {
-            let token = self.match_next(Ident)?;
-            let ident = token.to_str()
-                .expect("bug: idents should be valid utf-8 by definition");
-            names.push(ident)
-        }
-
-        Some(names)
-    }
-
-    fn ident_path(&mut self) -> Option<IdentPath<'a>> {
+    fn ident_path(&mut self) -> Result<IdentPath<'a>, Unrecoverable> {
         let mut components = Vec::new();
         loop {
             let token = self.lexer.next();
@@ -134,18 +168,18 @@ impl<'a> ModuleParser<'a> {
                 Semicolon => break,
                 _ => {
                     self.unexpected_token(token, &[DoubleColon, Semicolon]);
-                    return None;
+                    return Err(Unrecoverable);
                 },
             }
         }
 
-        Some(IdentPath {components})
+        Ok(IdentPath {components})
     }
 
-    fn path_component(&mut self, token: Token<'a>) -> Option<PathComponent<'a>> {
+    fn path_component(&mut self, token: Token<'a>) -> Result<PathComponent<'a>, Unrecoverable> {
         use Keyword::*;
 
-        Some(match &token.kind {
+        Ok(match &token.kind {
             Ident => {
                 let ident = token.to_str()
                     .expect("bug: idents should be valid utf-8 by definition");
@@ -161,22 +195,26 @@ impl<'a> ModuleParser<'a> {
                 let token = self.lexer.next();
                 self.unexpected_token(token, &[Ident, Keyword(Package), Keyword(SelfType),
                     Keyword(SelfValue), Keyword(Super)]);
-                return None;
+                return Err(Unrecoverable);
             },
         })
     }
 
-    fn match_next(&mut self, kind: TokenKind) -> Option<Token<'a>> {
+    fn match_next(&mut self, kind: TokenKind) -> Result<Token<'a>, Unrecoverable> {
         let token = self.lexer.next();
         if token.kind != kind {
             self.unexpected_token(token, &[kind]);
-            None
+            Err(Unrecoverable)
         } else {
-            Some(token)
+            Ok(token)
         }
     }
 
     fn unexpected_token(&self, token: Token<'a>, expected: &[TokenKind]) {
+        if token.kind == Error {
+            // No need to produce further errors on an error token
+            return;
+        }
         todo!()
     }
 }
