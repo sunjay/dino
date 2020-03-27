4722e9fbb4549710c2c26cd84da6326fe82c79c6
Sunjay Varma <varma.sunjay@gmail.com>
2020-03-27 12:12:00 -0400

    Defined corresponding method names for all operators

--- src/desugar/operator.rs
+++ src/desugar/operator.rs
@@ -11,7 +11,32 @@ impl Operator for ast::CompareOp {
     fn method_name(self) -> hir::Ident {
         use ast::CompareOp::*;
         match self {
-            _ => todo!()
+            Eq(span) => hir::Ident {
+                value: "eq".into(),
+                span,
+            },
+            Ne(span) => hir::Ident {
+                value: "ne".into(),
+                span,
+            },
+
+            //TODO: Replace this desugaring to use `Ord` and `Ordering`
+            Lt(span) => hir::Ident {
+                value: "lt".into(),
+                span,
+            },
+            Le(span) => hir::Ident {
+                value: "le".into(),
+                span,
+            },
+            Gt(span) => hir::Ident {
+                value: "gt".into(),
+                span,
+            },
+            Ge(span) => hir::Ident {
+                value: "ge".into(),
+                span,
+            },
         }
     }
 }
@@ -20,7 +45,26 @@ impl Operator for ast::BitwiseOp {
     fn method_name(self) -> hir::Ident {
         use ast::BitwiseOp::*;
         match self {
-            _ => todo!()
+            Or(span) => hir::Ident {
+                value: "bit_or".into(),
+                span,
+            },
+            Xor(span) => hir::Ident {
+                value: "bit_xor".into(),
+                span,
+            },
+            And(span) => hir::Ident {
+                value: "bit_and".into(),
+                span,
+            },
+            Shl(span) => hir::Ident {
+                value: "shl".into(),
+                span,
+            },
+            Shr(span) => hir::Ident {
+                value: "shr".into(),
+                span,
+            },
         }
     }
 }
@@ -29,7 +73,30 @@ impl Operator for ast::NumericOp {
     fn method_name(self) -> hir::Ident {
         use ast::NumericOp::*;
         match self {
-            _ => todo!()
+            Add(span) => hir::Ident {
+                value: "add".into(),
+                span,
+            },
+            Sub(span) => hir::Ident {
+                value: "sub".into(),
+                span,
+            },
+            Mul(span) => hir::Ident {
+                value: "mul".into(),
+                span,
+            },
+            Div(span) => hir::Ident {
+                value: "div".into(),
+                span,
+            },
+            Rem(span) => hir::Ident {
+                value: "rem".into(),
+                span,
+            },
+            Pow(span) => hir::Ident {
+                value: "pow".into(),
+                span,
+            },
         }
     }
 }
@@ -38,7 +105,18 @@ impl Operator for ast::UnaryOp {
     fn method_name(self) -> hir::Ident {
         use ast::UnaryOp::*;
         match self {
-            _ => todo!()
+            Pos(span) => hir::Ident {
+                value: "pos".into(),
+                span,
+            },
+            Neg(span) => hir::Ident {
+                value: "neg".into(),
+                span,
+            },
+            Not(span) => hir::Ident {
+                value: "not".into(),
+                span,
+            },
         }
     }
 }
