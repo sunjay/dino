6582581b42de29d1dd6d8caee46b0b7968a0b488
Sunjay Varma <varma.sunjay@gmail.com>
2020-04-11 11:43:03 -0400

    Moved package module in module list

--- src/lib.rs
+++ src/lib.rs
@@ -16,6 +16,7 @@ pub mod span;
 pub mod diagnostics;
 pub mod fmt_ctx;
 pub mod primitives;
+pub mod package;
 pub mod ast;
 pub mod parser;
 pub mod hir;
@@ -24,7 +25,6 @@ pub mod nir;
 pub mod resolve;
 pub mod cgenir;
 pub mod cir;
-pub mod package;
 
 use std::fs;
 use std::io;
