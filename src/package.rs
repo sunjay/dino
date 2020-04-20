8d9d023c677eaf5ce77c447def7673812504b61b
Sunjay Varma <varma.sunjay@gmail.com>
2020-04-20 16:07:32 -0400

    Defining std package with one module for now

--- src/package.rs
+++ src/package.rs
@@ -39,12 +39,43 @@ pub struct Module {
     pub functions: HashMap<Arc<str>, FuncSig>,
 }
 
+/// Inserts a function signature (no `self` parameter) into the given module
+#[macro_export]
+macro_rules! insert_sig {
+    (
+        $module:ident;
+        fn $func:ident(
+            $(self)? $(,)?
+            $($param:ident : $param_ty:expr),* $(,)?
+        ) -> $ret_ty:expr
+    ) => {
+        assert!($module.functions.insert(stringify!($func).into(), FuncSig {
+            self_param: false,
+            params: vec![
+                $(
+                    FuncParam {
+                        name: stringify!($param).into(),
+                        ty: $param_ty.into(),
+                    }
+                ),*
+            ],
+            return_type: $ret_ty.into(),
+        }).is_none(), "bug: function `{}` was inserted twice for primitive `{}`", stringify!($func), stringify!($prim));
+    };
+}
+// Makes this name available to import
+pub use crate::insert_sig;
+
 /// An ID for a type in a package
 #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub struct PkgTyId(usize);
+pub struct PkgTyId {
+    pkg: PkgId,
+    type_index: usize
+}
 
 #[derive(Debug)]
 pub struct Package {
+    id: PkgId,
     /// `PkgTyId` indexes into this field
     types: Vec<TypeDecl>,
     /// The root module in the package
@@ -53,17 +84,19 @@ pub struct Package {
 
 impl Package {
     pub fn insert_type(&mut self, type_decl: TypeDecl) -> PkgTyId {
-        let id = PkgTyId(self.types.len());
+        let id = PkgTyId {pkg: self.id, type_index: self.types.len()};
         self.types.push(type_decl);
         id
     }
 
     pub fn type_mut(&mut self, ty_id: PkgTyId) -> &mut TypeDecl {
-        let PkgTyId(index) = ty_id;
-        &mut self.types[index]
+        let PkgTyId {pkg, type_index} = ty_id;
+        assert_eq!(pkg, self.id, "bug: attempt to get type defined in another package");
+
+        &mut self.types[type_index]
     }
 
-    pub fn root(&mut self) -> &mut Module {
+    pub fn root_mut(&mut self) -> &mut Module {
         &mut self.root
     }
 }
@@ -85,6 +118,7 @@ impl Packages {
         let id = PkgId(self.packages.len());
 
         let package = Package {
+            id,
             types: Vec::new(),
             root: Module::default(),
         };
